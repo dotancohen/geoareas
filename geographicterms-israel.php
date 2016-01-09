a1653c2906f133aa902509beb7db81c448b14bdc
Dotan Cohen <github-com-2010.04@dotancohen.com>
2016-01-09 16:32:19 +0200

    Register the taxonomy

--- geographicterms-israel.php
+++ geographicterms-israel.php
@@ -9,19 +9,37 @@ License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+defined('ABSPATH') or die('Nice try!');
 
 
-register_activation_hook( __FILE__, 'gtisrael_activate' );
-function gtisrael_activate()
+add_action('init', 'dc_gt_israel_register_taxonomy');
+function dc_gt_israel_register_taxonomy()
 {
+	$labels = array(
+		'name'              => _x('Area', 'taxonomy general name'),
+		'singular_name'     => _x('Areas', 'taxonomy singular name'),
+		'search_items'      => __('Search Area'),
+		'all_items'         => __('All Areas'),
+		'parent_item'       => __('Parent Area'),
+		'parent_item_colon' => __('Parent Area:'),
+		'edit_item'         => __('Edit Area'),
+		'update_item'       => __('Update Area'),
+		'add_new_item'      => __('Add New Area'),
+		'new_item_name'     => __('New Area Name'),
+		'menu_name'         => __('Area')
+	);
 
-}
+	$args = array(
+		'hierarchical' => TRUE,
+		'labels'       => $labels,
+		'show_ui'      => TRUE,
+		'query_var'    => TRUE,
+		'rewrite'      => array( 'slug' => 'state' ),
+	);
 
+	$post_types = array('post', 'request', 'offering', 'page', 'partner');
 
-register_deactivation_hook( __FILE__, 'gtisrael_deactivate' );
-function gtisrael_deactivate()
-{
+	register_taxonomy('geographicterms-israel', $post_types, $args);
 
 }
 
